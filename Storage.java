7c17dc1996b6c05272e86f1f15db1a39a4e93576
Chaitanya <datar.chaitanya@gmail.com>
2025-06-23 15:30:01 -0400

    added way to remove a customer

--- Storage.java
+++ Storage.java
@@ -5,9 +5,15 @@ public class Storage{
 
     public void addCustomer(Customer customer){
         map.put(customer.getCustomerId(), customer);
+        System.out.println("Added Customer " + map.get(customer.getCustomerId()).getName());
     }
 
     public Customer getCustomer(int id){
         return(map.get(id));
+    }
+
+    public void removeCustomer(int id){
+        System.out.println("Removed Customer " + map.get(id).getName());
+        map.remove(id);
     }
 }
