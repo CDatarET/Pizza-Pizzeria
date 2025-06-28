4deb04dc9f0aa1cdb59b049ee01d0d2dd98b669e
Chaitanya <datar.chaitanya@gmail.com>
2025-06-28 00:28:26 -0400

    added new Pizza class

---
+++ Pizza.java
@@ -0,0 +1,49 @@
+public class Pizza {
+    private String name;
+    private String[] toppings;
+    private int cost;
+    private int[] tops;
+
+    public Pizza(String name, String[] toppings, int cost, int[] tops) {
+        this.name = name;
+        this.toppings = toppings;
+        this.cost = cost;
+        this.tops = tops;
+    }
+
+    // Getter and Setter for name
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter and Setter for toppings
+    public String[] getToppings() {
+        return toppings;
+    }
+
+    public void setToppings(String[] toppings) {
+        this.toppings = toppings;
+    }
+
+    // Getter and Setter for cost
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    // Getter and Setter for tops
+    public int[] getTops() {
+        return tops;
+    }
+
+    public void setTops(int[] tops) {
+        this.tops = tops;
+    }
+}
